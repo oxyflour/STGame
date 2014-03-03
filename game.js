9174071c809fe3cf5b49fe3d14e01a8395c6e7b1
Yangfu Fang <a6905688@gmail.com>
2014-03-03 22:00:35 +0800

    move DC save/restore back into object.draw
    nested save/restore is supported in html5 canvas, thus calling
    object.draw from another object.draw function is okay

--- game.js
+++ game.js
@@ -578,9 +578,7 @@ var GAME = (function() {
 	_t.draw = function() {
 		DC.clear();
 		SPRITE.eachLayerObj(function(i, v) {
-			DC.save();
 			v.draw();
-			DC.restore();
 		})
 	};
 	_t.keyste = {};
@@ -783,6 +781,7 @@ SPRITE.newCls('Basic', {
 	draw: function() {
 		var d = this.data, s = this.state;
 		if (d.opacity > 0) {
+			DC.save();
 			if (d.opacity < 1)
 				DC.globalAlpha = d.opacity;
 
@@ -792,6 +791,7 @@ SPRITE.newCls('Basic', {
 				this.drawFrame(d, s);
 			else if (d.text)
 				this.drawText(d, s);
+			DC.restore();
 		}
 	},
 	anim: function(t, fn, d, id) {
