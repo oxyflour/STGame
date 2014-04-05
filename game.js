833bb73c63d9f5d8859d876e40a31283f9358c15
oxyflour <a6905688@gmail.com>
2014-04-05 21:16:49 +0800

    add new canvas transform 'replacecolor'

--- game.js
+++ game.js
@@ -188,6 +188,56 @@ function redirect_object(from, to, v, f) {
 	return r;
 }
 
+// see http://stackoverflow.com/questions/2353211/hsl-to-rgb-color-conversion
+function hue2rgb(p, q, t) {
+	if (t < 0)
+		t += 1;
+	if (t > 1)
+		t -= 1;
+	if (t < 1/6)
+		return p + (q - p) * 6 * t;
+	if (t < 1/2)
+		return q;
+	if (t < 2/3)
+		return p + (q - p) * (2/3 - t) * 6;
+	return p;
+}
+function hsl2rgb(h, s, l) {
+	var r, g, b;
+	if (!s) {
+		r = g = b = l; // achromatic
+	}
+	else {
+		var q = l < 0.5 ? l * (1 + s) : 1 + s - l * s,
+			p = 2 * l - q;
+		r = hue2rgb(p, q, h + 1/3);
+		g = hue2rgb(p, q, h);
+		b = hue2rgb(p, q, h - 1/3);
+	}
+	return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+}
+function rgb2hsl(r, g, b) {
+	r /= 255, g /= 255, b /= 255;
+	var max = Math.max(r, g, b),
+		min = Math.min(r, g, b);
+	var h, s, l = (max + min) / 2;
+
+	if (max == min) {
+		h = s = 0; // achromatic
+	}
+	else {
+		var d = max - min;
+		s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+		switch (max) {
+			case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+			case g: h = (b - r) / d + 2; break;
+			case b: h = (r - g) / d + 4; break;
+		}
+		h /= 6;
+	}
+	return [h, s, l];
+}
+
 function $(s, p) {
 	return (p || document).querySelectorAll(s);
 }
@@ -438,6 +488,22 @@ var RES = (function(res) {
 				dc.globalCompositeOperation = 'source-atop';
 				dc.fillStyle = t.v;
 				dc.fillRect(0, 0, sc.w, sc.h);
+			}
+			else if (t.k == 'replacecolor') {
+				dc.drawImage(sc.s, sc.x, sc.y, sc.w, sc.h,
+					0, 0, sc.w, sc.h);
+				var im = dc.getImageData(0, 0, sc.w, sc.h),
+					d = im.data,
+					h = rgb2hsl(0, 255, 255)[0];
+				for (var i = 0; i < d.length; i += 4) {
+					hsl = rgb2hsl(d[i], d[i+1], d[i+2]);
+					hsl[0] = h;
+					rgb = hsl2rgb(hsl[0], hsl[1], hsl[2]);
+					d[i] = rgb[0];
+					d[i+1] = rgb[1];
+					d[i+2] = rgb[2];
+				}
+				dc.putImageData(im, 0, 0);
 			}
 			dc.restore();
 		});
