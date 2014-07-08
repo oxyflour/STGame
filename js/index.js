6caf4f2c2ff0d5bedef4a4f6886e77798b359b38
oxyflour <a6905688@gmail.com>
2014-07-08 21:41:46 +0800

    update text

--- js/index.js
+++ js/index.js
@@ -185,8 +185,7 @@ GAME.init();
 GAME.state = GAME.states.LOADING;
 
 RES.check(function() {
-	console.log('咦，你想做什么奇怪的事情吗？');
-	console.log('找秘籍的话，在主页面第950行附近哦');
+	console.log('咦，你想找奇怪的东西吗？（秘籍在主页面第950行附近哦）');
 	console.log('对源代码感兴趣的话，直接到 https://github.com/oxyflour/stgame 去 pull 吧');
 	console.log('虽然现在是三无产品（无设计，无文档，无测试），有人想一起玩的话我会尽量补上的');
 });
